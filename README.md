7badb2b4d60e3a773ba94f2cb96d21548891e39a
Jack <j-bass@outlook.com>
2024-09-06 19:58:12 +0100

    Update three.py to use functions and name == main

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 ### Get puzzled at [pythonchallenge.com](pythonchallenge.com). Note, I did not make Python Challenge, these are merely my answers to the challenge!
 
 Looking at my answers will spoil your experience of Python Challenge if you haven't attempted the puzzles yourself. You have been warned.
+
+### How to run
+Simply execute `python -m levels.one` to run the script for the first level, and so on.
--- levels/three.py
+++ levels/three.py
@@ -1,47 +1,63 @@
 import re
+
 from data.three_text import source as in_str
 
 
-length = len(in_str)
-out_str = ''
+# level url: http://www.pythonchallenge.com/pc/def/equality.html
 
-# Initial attempt using for loops
-# Convert input string into list separated at newlines
-in_list = in_str.splitlines()
+# Initial attempt using for loops. Yes it gets ugly.
+def three_looped():
+    out_str = ''
 
-# Assume lists are same length
-list_length = len(in_list[1])
-print(f"{list_length=}")
+    # Convert input string into list separated at newlines
+    in_list = in_str.splitlines()
 
-for string_num, current_string in enumerate(in_list):
-    # Skip blank line
-    if string_num == 0:
-        continue
-    for i in range(list_length):
-        # Stop when we get to the end of the string
-        if i + 7 >= list_length:
+    # Assume lists are same length
+    list_length = len(in_list[1])
+    print(f"{list_length=}")
+
+    for string_num, current_string in enumerate(in_list):
+        # Skip blank line
+        if string_num == 0:
             continue
-        if current_string[i].isupper():
-            if current_string[i+1].isupper():
-                if current_string[i+2].isupper():
+        for i in range(list_length):
+            # Stop when we get to the end of the string
+            if i + 7 >= list_length:
+                continue
+            if current_string[i].isupper():
+                if current_string[i+1].isupper():
+                    if current_string[i+2].isupper():
 
-                    if current_string[i+3].islower():
+                        if current_string[i+3].islower():
 
-                        if current_string[i+4].isupper():
-                            if current_string[i+5].isupper():
-                                if current_string[i+6].isupper():
+                            if current_string[i+4].isupper():
+                                if current_string[i+5].isupper():
+                                    if current_string[i+6].isupper():
 
-                                    # Does it have to have lowercase either side?
-                                    if current_string[i+-1].islower():
-                                        if current_string[i+7].islower():
-                                            out_str += current_string[i+3]
+                                        # Does it have to have lowercase either side?
+                                        if current_string[i+-1].islower():
+                                            if current_string[i+7].islower():
+                                                out_str += current_string[i+3]
 
-print(f'{out_str=}')
+    return out_str
 
-# Attempt using regex
-pattern = r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]'
-middle_letters = re.findall(pattern, in_str)
 
-out_str_regex = ''.join(middle_letters)
+def three_regex():
+    # Attempt using regex
+    pattern = r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]'
+    middle_letters = re.findall(pattern, in_str)
 
-print(f'{out_str_regex=}')
+    out_str = ''.join(middle_letters)
+
+    return out_str
+
+
+def main():
+
+    print(f"Looped answer: {three_looped()}")
+
+    print(f"Regex answer: {three_regex()}")
+
+
+if __name__ == '__main__':
+    main()
