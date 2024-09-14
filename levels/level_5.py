792b31fa5599328e1c0fbff2f426ca7261396927
Jack <j-bass@outlook.com>
2024-09-14 17:04:10 +0100

    Cleanup level 5

--- levels/level_5.py
+++ levels/level_5.py
@@ -14,39 +14,15 @@ image_path = Path("data/banner.p")
 
 def pickle_image(img_path):
 
-    # unpickled = pickle.load(image_path)
-    # unprint(f"{pickled=}")
-
-    answer = 0
-
     with open(img_path, 'rb') as f:
 
         unpickled = pickle.load(f, fix_imports=True)
-        print(f"{unpickled=}")
-
-        char_num = 0
+        # print(f"{unpickled=}")
 
         for list in unpickled:
-            # print(list)
-            
-            total = 0
             for char, num in list:
                 print(char * num, end='')
             print()
-                # total += num
-                # if char == "#":
-                #     char_num += num
-            # answer += char_num
-            # print(f"{chr(char_num)=}")
-            
-            # All rows add up to 95
-            # if total != 95:
-            #     print(f"{list=}")
-
-        # print(f"{answer=}")
-        # The answer is not 661
-        # The answer is not 5942
-        # The answer is not 20278
 
 
 def main():
