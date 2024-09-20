575d1193090c8b1e863ec4bfd291d6d47b5a3796
Jack <j-bass@outlook.com>
2024-09-20 19:35:30 +0100

    L6 WIP

--- levels/level_6.py
+++ levels/level_6.py
@@ -1,15 +1,43 @@
 from pathlib import Path
 import re
 import sys
+import os
+import zipfile
 
 
-# level url: www.pythonchallenge.com/pc/def/channel.zip
+# level url: www.pythonchallenge.com/pc/def/channel.html
+# Dowload zip file at www.pythonchallenge.com/pc/def/channel.zip
 # Hints
-# From data/channel/readme.txt: 
+# From data/channel/readme.txt:
 # hint1: start from 90052
 # hint2: answer is inside the zip
 
+# Loop stops at 46145.txt text below:
+# Collect the comments.
+
 file_key = 90052
+
+
+def search_files(directory):
+    pattern = r"\d+$"
+    for file in os.listdir(directory):
+        with open(os.path.join(directory, file), "r") as f:
+            for line in f:
+                m = re.search(pattern, line)
+                if not m:
+                    print(f"{line}")
+
+
+def get_zip_comment(zip_file_path):
+
+    file_comments = {}
+
+    zip_file = zipfile.ZipFile(zip_file_path)
+    infolist = zip_file.infolist()
+    for info in infolist:
+        file_comments[info.filename] = str(info.comment, "utf-8")
+
+    return file_comments
 
 
 def traverse_files(file_key):
@@ -19,19 +47,26 @@ def traverse_files(file_key):
 
     filepath = Path(f"data/level_6/channel/{file_key}.txt")
 
-    with open(filepath, "r") as f:
-        for line in f:
-            m = re.search(pattern, line)
-            if m:
-                print(line)
-                file_key = m.group(0)
-                filepath = Path(f"data/level_6/channel/{file_key}.txt")
-            else:
-                print(f"Failed to find the next key. Page text: {line}")
+    while True:
+        with open(filepath, "r") as f:
+            for line in f:
+                m = re.search(pattern, line)
+                if m:
+                    print(line)
+                    file_key = m.group(0)
+                    filepath = Path(f"data/level_6/channel/{file_key}.txt")
+                else:
+                    print(f"Failed to find the next key. Page text: {line}")
+                    sys.exit()
 
 
 def main():
-    traverse_files(file_key)
+    # traverse_files(file_key)
+
+    path = Path("data/level_6/channel.zip")
+
+    output = get_zip_comment(path)
+    print(output)
 
 
 if __name__ == "__main__":
