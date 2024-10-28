1f16a5355fac7fc288ec3629ca95e3d91692521e
Jack <j-bass@outlook.com>
2024-10-28 19:15:41 +0000

    Level 16 init

---
+++ levels/level_16.py
@@ -0,0 +1,17 @@
+# level url: http://www.pythonchallenge.com/pcc/return/mozart.html
+# Level title: "let me get this straight"
+# Hint: Doesn't seem to be a hint.
+
+# Image is a TV static looking picture with some pink marks on
+# It is a gif
+
+# What do we have to straighten out?
+
+# What does Mozart have to do with it? Do we have to 'compose' something?
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
